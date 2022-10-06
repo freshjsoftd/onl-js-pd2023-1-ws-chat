bdc4b1e17bfce61e29c5ddb85ea8f28e92353828
yan <yan.braverman37@gmail.com>
2022-10-06 14:52:53 +0300

    Prepare for lesson

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import store from "./store";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+    </Provider>
+  </React.StrictMode>
 );
